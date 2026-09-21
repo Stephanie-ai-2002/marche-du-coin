841fab995678b3acd3b10ca04cf9715b5a9d4b0a
HOUNKPATIN Stéphanie <stephaniehounkpatin.2002@gmail.com>
2026-09-21 20:48:34 +0200

    Correction des imports manquants dans AuthController (Request et Hash)

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\UpdateProfilRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
